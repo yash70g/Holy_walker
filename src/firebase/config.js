189db7d9c6d1fdb397291c7ce5a6c97d4a469608
Yash Gupta <145326279+yash70g@users.noreply.github.com>
2025-12-26 01:35:49 +0530

    shit i messed config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,7 @@ import { initializeFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3X6yx_3cRycIaQkxI3EB8Q3ZuSPSLrqE",
-  authDomain: "territory-walk-game.firebaseapp.com",
-  projectId: "territory-walk-game",
-  storageBucket: "territory-walk-game.firebasestorage.app",
-  messagingSenderId: "992107497450",
-  appId: "1:992107497450:web:6ae31f31b62e4b84b2d493",
-  measurementId: "G-B2J0BG78FE"
+  
 };
 
 const app = initializeApp(firebaseConfig);
